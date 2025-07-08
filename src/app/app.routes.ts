5ab9f8a563acc2f597a5e3e5b2f12085a38b51e5
Jess JH <89726090+Icelii@users.noreply.github.com>
2025-07-08 05:39:26 -0600

    feat: crear interfaz Home con Navbar y Footer
    Se implementó la estructura básica de la interfaz de la página de inicio.
    
    Se creó e implementó la barra de navegación (Navbar) y el pie de página (Footer).

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,16 +6,18 @@ import { RegisterComponent } from './features/auth/pages/register/register.compo
 import { AuthCodeComponent } from './features/auth/pages/auth-code/auth-code.component';
 import { RecoveryAccountComponent } from './features/auth/pages/recovery-account/recovery-account.component';
 import { NewPasswordComponent } from './features/auth/pages/new-password/new-password.component';
-import { AccountVerifiedLayoutComponent } from './layouts/account-verified-layout/account-verified-layout.component';
-import { ButtonAccountVerifyComponent } from './features/auth/components/button-account-verify/button-account-verify.component';
-import { ButtonAccountFailComponent } from './features/auth/components/button-account-fail/button-account-fail.component';
+import { HomeComponent } from './features/home/pages/home/home.component';
+import { UserDashboardLayoutComponent } from './layouts/user-dashboard-layout/user-dashboard-layout.component';
+import { AdoptComponent } from './features/adopt/pages/adopt/adopt.component';
 
 export const routes: Routes = [
-    {
+  {
     path: '',
     component: MainLayoutComponent,
     children: [
-    ]
+      { path: '', component: HomeComponent },
+      { path: 'adopt', component: AdoptComponent }
+    ],
   },
   {
     path: '',
@@ -25,16 +27,13 @@ export const routes: Routes = [
       { path: 'register', component: RegisterComponent },
       { path: 'auth-code', component: AuthCodeComponent },
       { path: 'account-recovery', component: RecoveryAccountComponent },
-      { path: 'new-password', component: NewPasswordComponent }
-    ]
+      { path: 'new-password', component: NewPasswordComponent },
+    ],
   },
   {
-    path: '',
-    component: AccountVerifiedLayoutComponent,
-    children: [
-      { path: 'verification-success', component: ButtonAccountVerifyComponent },
-      { path: 'verification-fail', component: ButtonAccountFailComponent },
-    ]
+    path: 'test',
+    component: UserDashboardLayoutComponent,
+    children: [],
   },
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: '' },
 ];
--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from "../../shared/components/nav-bar/nav-bar.component";
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "../../shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent, RouterOutlet, FooterComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })
