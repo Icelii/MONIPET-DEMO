cb3d8a7d5afb536b7dce0f7fcd5729a005970cf2
Jess JH <89726090+Icelii@users.noreply.github.com>
2025-08-15 01:53:33 -0600

    feat(services): Funcionalidad de servicios
    Se implementa catalogo de servicios.
    
    Se implementa agendar citas.

--- src/app/features/services/pages/appointment/appointment.component.ts
+++ src/app/features/services/pages/appointment/appointment.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
 import { ServiceAppointmentCardComponent } from "../../components/service-appointment-card/service-appointment-card.component";
 import { AppointmentDetailsComponent } from "../../../../shared/components/appointment-details/appointment-details.component";
+import { Subscription, take, timeout } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ServiceService } from '../../../../core/services/services/service.service';
 
 @Component({
   selector: 'app-appointment',
@@ -9,6 +12,73 @@ import { AppointmentDetailsComponent } from "../../../../shared/components/appoi
   templateUrl: './appointment.component.html',
   styleUrl: './appointment.component.css'
 })
-export class AppointmentComponent {
+export class AppointmentComponent implements OnInit, OnDestroy {
+  services: number[] = [];
+  servicesSelected = signal<any[]>([]);
+  loading = signal(true);
+  private queryParamsSub!: Subscription;
+  selectedServiceIds: number[] = [];
+  typeAppointment: string = Math.random() < 0.5 ? 'Estetica' : 'Medica';
 
+  constructor(private router: Router, private route: ActivatedRoute, private ServiceService: ServiceService) {}
+
+  ngOnInit(): void {
+    this.queryParamsSub = this.route.queryParams.subscribe((params: any) => {
+      this.services = params['services'] ? JSON.parse(params['services']) : [];
+      console.log('IDs servicios: ', this.services);
+      this.getServicesList();
+    });
+  }
+
+  getServicesList() {
+    this.ServiceService.getServiceList(this.services).pipe(timeout(15000), take(1)).subscribe({
+      next: (response) => {
+        if (response.result) {
+          this.servicesSelected.set(response.data);
+          this.selectedServiceIds = response.data.map((s: any) => ({
+            service_id: s.id,
+            price: s.price,
+            discount: s.discounts ?? 0.00
+          }));
+          console.log('SERVICIOS PARA LA CITA: ', response.data);
+        } else {
+          this.servicesSelected.set([]);
+          this.selectedServiceIds = [];
+        }
+        this.loading.set(false);
+      },
+      error: (error) => {
+        this.servicesSelected.set([]);
+        this.selectedServiceIds = [];
+        this.loading.set(false);
+        console.log(error);
+      }
+    });
+  }
+
+  get total(): number {
+    return this.servicesSelected().reduce((acc, service) => {
+      const discount = service.discounts && service.discounts > 0 ? service.discounts : 0;
+      const priceDiscounted = service.price * (1 - discount / 100);
+      return acc + priceDiscounted;
+    }, 0);
+  }
+
+  get totalDiscount(): number {
+    return this.servicesSelected().reduce((acc, service) => {
+      const discount = service.discounts && service.discounts > 0 ? service.discounts : 0;
+      const discountAmount = service.price * (discount / 100);
+      return acc + discountAmount;
+    }, 0);
+  }
+
+  get subtotal(): number {
+    return this.servicesSelected().reduce((acc, service) => acc + parseFloat(service.price), 0);
+  }
+
+  ngOnDestroy(): void {
+    if (this.queryParamsSub) {
+      this.queryParamsSub.unsubscribe();
+    }
+  }
 }
--- src/app/features/services/pages/services/services.component.ts
+++ src/app/features/services/pages/services/services.component.ts
@@ -1,19 +1,30 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, computed, OnInit, signal } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ServiceCardComponent } from "../../components/service-card/service-card.component";
+import { ServiceService } from '../../../../core/services/services/service.service';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import {NgxPaginationModule} from 'ngx-pagination';
+import { take, timeout } from 'rxjs';
+import { LoaderElementsComponent } from '../../../../shared/components/loader-elements/loader-elements.component';
+import { addServiceToSelection, removeServiceFromSelection, selectedServices } from '../../../../core/stores/appointments.store';
+import { AuthService } from '../../../../core/services/auth.service';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, ServiceCardComponent],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, ServiceCardComponent, RouterModule, NgxPaginationModule, LoaderElementsComponent],
   templateUrl: './services.component.html',
   styleUrl: './services.component.css',
 })
-export class ServicesComponent {
+export class ServicesComponent implements OnInit {
+  p: number = 1;
   filtrosForm: FormGroup;
   dropdownOpen = false;
+  services: any[] = [];
+  serviciosSeleccionados= computed(() => selectedServices());
   options = [false, false, false];
+  loading = signal(true);
 
   categorias = [
     {
@@ -24,13 +35,41 @@ export class ServicesComponent {
     },
   ];
 
-  constructor(private fb: FormBuilder) {
+  constructor(private authService: AuthService, private fb: FormBuilder, private serviceService: ServiceService, private router: Router) {
     this.filtrosForm = this.fb.group({});
   }
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    this.getServices();
+  }
+
+  onServiceSelectionChange(event: { id: number; selected: boolean }) {
+    if (event.selected) {
+      addServiceToSelection(event.id);
+      console.log('SELECTED: ', this.serviciosSeleccionados());
+    } else {
+      removeServiceFromSelection(event.id);
+    }
+  }
 
   onSubmit() {}
+
+  getServices() {
+    this.serviceService.getServices().pipe(timeout(15000), take(1)).subscribe({
+      next: (response) => {
+        if (response.result) {
+          this.services = response.data;
+          this.loading.set(false);
+        } else {
+          this.loading.set(false);
+        }
+      },
+      error: (error) => {
+        this.loading.set(false);
+        console.log(error);
+      }
+    });
+  }
 
   toggleDropdown(index: number) {
     this.categorias[index].open = !this.categorias[index].open;
@@ -39,5 +78,34 @@ export class ServicesComponent {
   onCheck(catIndex: number, optIndex: number, event: Event) {
     const input = event.target as HTMLInputElement;
     this.categorias[catIndex].seleccionadas[optIndex] = input.checked;
+  }
+
+  get selectedServiceNames(): string[] {
+    return this.serviciosSeleccionados()
+      .map((id: any) => this.services.find(s => s.id === id))
+      .filter((s: any) => s)
+      .map((s: any) => s!.service);
+  }
+
+  executeIfCanSchedule(action: () => void) {
+    if (this.serviciosSeleccionados().length === 0) {
+      alert('Debes seleccionar al menos un servicio');
+      return;
+    }
+
+    if (!this.authService.isAuthenticated()) {
+      this.router.navigate(['/login']);
+      return;
+    }
+
+    action();
+  }
+
+  scheduleAppointment() {
+    this.executeIfCanSchedule(() => {
+      this.router.navigate(['/services/schedule-service'], {
+        queryParams: { services: JSON.stringify(this.serviciosSeleccionados()) }
+      });
+    });
   }
 }
