f967cccee3940a795e4a620298c5ef3828d79b7c
Jess JH <89726090+Icelii@users.noreply.github.com>
2025-08-15 02:01:22 -0600

    feat(dashboard orders): Funcionalidad completa

--- src/app/features/dashboard/components/order-card/order-card.component.ts
+++ src/app/features/dashboard/components/order-card/order-card.component.ts
@@ -1,18 +1,79 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { OrdenService } from '../../../../core/services/orders/orden.service';
+import { FormBuilder, FormGroup, FormsModule } from '@angular/forms';
+import { take, timeout } from 'rxjs';
+import Swal from 'sweetalert2';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-order-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './order-card.component.html',
   styleUrl: './order-card.component.css'
 })
 export class OrderCardComponent {
   @Input() id: number = 0;
-  @Input() purchase_date: string = '';
+  @Input() purchase_date: string | null = null;
   @Input() price: number = 0;
   @Input() status: string = '';
   @Input() quantity: number = 0;
-  @Input() producto_id: number = 0;
+  @Input() photos: string[] = [];
+  @Output() updated = new EventEmitter<void>();
+  cancelOrderForm: FormGroup;
+
+  constructor(private ordenService: OrdenService, private fb: FormBuilder, private router: Router) {
+    this.cancelOrderForm = this.fb.group({
+      'status': ['Cancelada']
+    });
+  }
+
+  onOrderUpdated() {
+    this.updated.emit();
+  }
+
+  confirmCancel() {
+    Swal.fire({
+      title: "¿Estas seguro de cancelar esta orden?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#489dba",
+      cancelButtonColor: "#a53f3f",
+      confirmButtonText: "Si, cancelar",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+         this.cancelOrder();
+      }
+    });
+  }
+
+  cancelOrder() {
+    const data = this.cancelOrderForm.value;
+
+    this.ordenService.cancelOrder(this.id, data).pipe(timeout(15000), take(1)).subscribe({
+      next: (response) => {
+        if (response.result) {
+          if (response.result) {
+            Swal.fire({
+              title: "¡Orden cancelada!",
+              icon: "success",
+              timer: 1500,
+              showConfirmButton: false
+              }).then((result) => {
+                this.updated.emit();
+            });
+          }
+        }
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+  }
+
+  goToDetails() {
+    this.router.navigate(['dashboard/orders/detail', this.id]);
+  }
 }
--- src/app/features/dashboard/pages/orders/orders.component.ts
+++ src/app/features/dashboard/pages/orders/orders.component.ts
@@ -1,13 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, OnInit, signal } from '@angular/core';
 import { OrderCardComponent } from "../../components/order-card/order-card.component";
+import { userOrders } from '../../../../core/stores/orders.store';
+import { OrdenService } from '../../../../core/services/orders/orden.service';
+import { currentUser } from '../../../../core/stores/auth.store';
+import { take, timeout } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { LoaderElementsComponent } from '../../../../shared/components/loader-elements/loader-elements.component';
 
 @Component({
   selector: 'app-orders',
   standalone: true,
-  imports: [OrderCardComponent],
+  imports: [CommonModule, OrderCardComponent, NgxPaginationModule, LoaderElementsComponent],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.css'
 })
-export class OrdersComponent {
+export class OrdersComponent implements OnInit {
+  user = computed(() => currentUser());
+  orders = computed(() => userOrders());
+  p: number = 1;
+  loading = signal(true);
+  searchText = signal('');
 
+  allOrders = computed(() => {
+    const orders = this.orders();
+    if (!orders) return [];
+
+    return orders.map((order: any) => {
+      const totalQuantity = order.details.reduce((acc: number, detail: any) => acc + detail.quantity, 0);
+      return {
+        ...order,
+        totalQuantity
+      };
+    });
+  });
+
+  constructor(private ordenService: OrdenService) {
+    effect(() => {
+      const user = this.user();
+    
+      if (user?.id) {
+        this.getOrders();
+      }
+    });
+  }
+
+  ngOnInit(): void {}
+
+  getOrders() {
+    this.ordenService.getOrders(this.user().id).pipe(timeout(15000), take(1)).subscribe({
+      next: (response) => {
+        if (response.result) {
+          userOrders.set(response.data);
+          this.loading.set(false);
+        } else {
+          userOrders.set([]);
+          this.loading.set(false);
+        }
+      },
+      error: (error) => {
+        userOrders.set([]);
+        this.loading.set(false);
+        console.log(error);
+      }
+    });
+  }
+
+  getTwoRandomPhotos(order: any): string[] {
+    const allPhotos = order.details.flatMap((detail: any) =>
+      detail.product.product_photos.map((photo: any) => photo.photo_link)
+    );
+
+    if (allPhotos.length <= 2) {
+      return allPhotos;
+    }
+
+    const selectedPhotos: string[] = [];
+    while (selectedPhotos.length < 2) {
+      const randomIndex = Math.floor(Math.random() * allPhotos.length);
+      const photo = allPhotos[randomIndex];
+      if (!selectedPhotos.includes(photo)) {
+        selectedPhotos.push(photo);
+      }
+    }
+
+    return selectedPhotos;
+  }
 }
