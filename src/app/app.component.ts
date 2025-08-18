ef18dcb68f9f37dd9bf87c70d7b2a3415638d4db
Jess JH <89726090+Icelii@users.noreply.github.com>
2025-08-18 16:14:03 -0600

    fix6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { CommonModule } from '@angular/common';
-import { Component, inject, NgZone } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, Inject, inject, NgZone, PLATFORM_ID } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { UserService } from './core/services/user.service';
-import { take, timeout } from 'rxjs';
+import { filter, take, timeout } from 'rxjs';
 import { UserBase } from './core/interfaces/user';
 import { AuthService } from './core/services/auth.service';
 import { LoaderComponent } from "./shared/components/loader/loader.component";
@@ -18,7 +18,23 @@ export class AppComponent {
   private auth = inject(AuthService);
   title = 'MoniPet';
 
+  constructor(
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
   ngOnInit() {
     this.auth.getUserInfo();
+
+    if (isPlatformBrowser(this.platformId)) {
+      this.router.events
+        .pipe(filter(event => event instanceof NavigationEnd))
+        .subscribe(() => {
+          document.body.style.overflow = '';
+          document.body.classList.remove('offcanvas-backdrop', 'modal-open');
+          const backdrop = document.querySelector('.offcanvas-backdrop');
+          if (backdrop) backdrop.remove();
+        });
+    }
   }
 }
