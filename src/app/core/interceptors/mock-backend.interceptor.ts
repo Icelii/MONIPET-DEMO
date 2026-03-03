0797fb35b18b5af5ab0fd5593ca8f548a07356f3
JaretEduardo <jaret.gonzalez.ct59@gmail.com>
2026-03-03 13:42:18 -0600

    feat: products and pets

--- src/app/core/interceptors/mock-backend.interceptor.ts
+++ src/app/core/interceptors/mock-backend.interceptor.ts
@@ -7,30 +7,35 @@ export const mockBackendInterceptor: HttpInterceptorFn = (req, next) => {
 
     let mockUrl = '';
 
-    if (normalizedPath === 'pets') {
-      mockUrl = '/mock-data/pets.json';
-    }
-    else if (normalizedPath === 'types_pet') {
-      mockUrl = '/mock-data/types_pet.json';
-    }
-    else if (normalizedPath === 'breeds') {
-      mockUrl = '/mock-data/breeds.json';
-    }
-    else if (normalizedPath === 'pets/list') {
-      mockUrl = '/mock-data/pets/list.json';
-    }
+    if (normalizedPath === 'pets') mockUrl = '/mock-data/pets.json';
+    else if (normalizedPath === 'types_pet') mockUrl = '/mock-data/types_pet.json';
+    else if (normalizedPath === 'breeds') mockUrl = '/mock-data/breeds.json';
+
+    else if (normalizedPath === 'products') mockUrl = '/mock-data/products.json';
+    else if (normalizedPath === 'types_products') mockUrl = '/mock-data/types_products.json';
+    else if (normalizedPath === 'categories') mockUrl = '/mock-data/categories.json';
+    else if (normalizedPath === 'products/list') mockUrl = '/mock-data/products/list.json';
+    else if (normalizedPath === 'pets/list') mockUrl = '/mock-data/pets/list.json';
+
     else if (normalizedPath.startsWith('pets/adopter/')) {
       mockUrl = '/mock-data/pets.json';
     }
     else if (normalizedPath.startsWith('pets/')) {
-      const petId = normalizedPath.split('pets/')[1];
-      if (/^\d+$/.test(petId)) {
-        mockUrl = `/mock-data/pets/${petId}.json`;
+      const param = normalizedPath.split('pets/')[1];
+      if (/^\d+$/.test(param)) {
+        mockUrl = `/mock-data/pets/${param}.json`;
+      }
+    }
+    
+    else if (normalizedPath.startsWith('products/')) {
+      const param = normalizedPath.split('products/')[1];
+      if (/^\d+$/.test(param)) {
+        mockUrl = `/mock-data/products/${param}.json`;
       }
     }
 
     if (mockUrl !== '') {
-      console.log(`[Mock Interceptor] Redirigiendo ${req.url} -> ${mockUrl}`);
+      console.log(`[Mock Interceptor] Redirigiendo ${req.url} -> ${mockUrl}`); 
       
       const mockReq = req.clone({
         url: mockUrl,
